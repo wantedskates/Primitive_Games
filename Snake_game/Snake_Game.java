bd012ba1c9d22abac337efee4468a0bfaeb3249b
Mustafa Mohamed <Mustafamoham46@gmail.com>
2026-09-26 23:34:12 +0300

    Add files via upload

---
+++ Snake_game/Snake_Game.java
@@ -0,0 +1,30 @@
+public class Snake_Game {
+
+    public class Engines{
+    }
+    public class Poison{
+        String name;
+        long damage;
+        public class Vanilla{}
+        public class Cacao{}
+
+    }
+    public class Food {
+
+        String name;
+        long  price;
+        public class Apple{}
+        public class Banana{}
+        public class Watermelon{}
+        public class fastFood{}
+
+
+
+
+    }
+    public class Snake{
+
+        int maxSpeed;
+        
+    }
+}
